d6327792ca47b4d3fbc6cc7f30d10edb1eb19b6c
Michael Seifert <m.seifert@digitalernachschub.de>
2018-09-12 15:17:38 +0200

    BarChart displays value labels above bars.

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -2,6 +2,7 @@
   <svg :width="width" :height="height">
     <svg v-for="(point, index) in data" :key="index" :width="barWidth" :x="index*(barWidth + gapWidth)">
       <rect :y="plotAreaHeight - (point.value/maxValue)*plotAreaHeight" width="100%" :height="point.value/maxValue*plotAreaHeight" />
+      <text x="50%" :y="plotAreaHeight - (point.value/maxValue)*plotAreaHeight-fontSize/2" alignment-baseline="middle" text-anchor="middle">{{point.value}}</text>
       <text x="50%" :y="labelBaselineY" alignment-baseline="middle" text-anchor="middle">{{point.label}}</text>
     </svg>
   </svg>
