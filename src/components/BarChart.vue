ea45e883e1ceeb54428493c402ad0e40f6af0109
Michael Seifert <m.seifert@digitalernachschub.de>
2018-09-12 14:03:32 +0200

    BarChart dynamically computes the bar width based on the gap width.

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,7 +1,7 @@
 <template>
   <svg :width="width" :height="height">
     <g v-for="(point, index) in data" :key="index" >
-      <rect :x="index*110" :y="400-(point.value/maxValue)*400" width="100" :height="point.value/maxValue*400" />
+      <rect :x="index*(barWidth + gap)" :y="400-(point.value/maxValue)*400" :width="barWidth" :height="point.value/maxValue*400" />
     </g>
   </svg>
 </template>
@@ -12,11 +12,18 @@ export default {
   props: {
     width: {type: Number, required: true},
     height: {type: Number, required: true},
-    data: {type: Array, required: true}
+    data: {type: Array, required: true},
+    gap: {type: Number, default: 10}
   },
   computed: {
     maxValue: function() {
       return Math.max(...this.data.map(v => v.value))
+    },
+    barWidth: function() {
+      return (this.width - this.gapCount*this.gap) / this.data.length
+    },
+    gapCount: function() {
+      return this.data.length - 1
     }
   }
 }
